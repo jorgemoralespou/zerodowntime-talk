50a690609a63942f56e503d771ab5d4e58cbe9e5
Jorge Morales Pou <jorgemoralespou@gmail.com>
2017-01-10 19:16:21 +0100

    Added 2 endpoints to set return error for probes

--- probes-logger-python/wsgi.py
+++ probes-logger-python/wsgi.py
@@ -6,6 +6,8 @@ import json
 from flask import Flask, request
 from flask_restful import Resource, Api
 
+READYRET='OK'
+LIVERET='OK'
 
 application = Flask(__name__)
 
@@ -13,12 +15,31 @@ api = Api(application)
 
 class ReadyCheck(Resource):
     def get(self):
-        return 'OK'
+        return READYRET
 
 api.add_resource(ReadyCheck, '/ws/ready')
 
 class LiveCheck(Resource):
     def get(self):
-        return 'OK'
+        return LIVERET
 
 api.add_resource(LiveCheck, '/ws/live')
+
+
+class ReadyUnset(Resource):
+    global READYRET
+    
+    def get(self):
+        READYRET='NOK'
+        return 'OK'
+
+api.add_resource(ReadyUnset, '/ws/readynot')
+
+class LiveUnset(Resource):
+    global LIVERET
+
+    def get(self):
+        LIVERET='NOK'
+        return 'OK'
+
+api.add_resource(LiveUnset, '/ws/livenot')
